db0a17e4aa9b320d366ee27bd607e0bb8e863676
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-04-13 09:08:24 -0700

    update scraper after ig changes

--- functions/fetchAccountId.js
+++ functions/fetchAccountId.js
@@ -2,8 +2,21 @@ const fetchAccountId = (igAccount) => {
 	return new Promise( async (resolve, reject) => {
 		try {
 			const requestPromise = require('request-promise-native')
-			const accountInfo = await requestPromise(`https://instagram.com/${igAccount}/?__a=1`)
-			resolve(JSON.parse(accountInfo).graphql.user.id)
+			const getFromCookieFile = require('./getFromCookieFile')
+			const { users } = JSON.parse(await requestPromise({
+				url: `https://i.instagram.com/api/v1/users/search/?&q=${igAccount}`,
+				headers: {
+					'x-ig-capabilities': '3brTAw==',
+					'user-agent': 'Instagram 10.26.0 (iPhone7,2; iOS 10_1_1; en_US; en-US; scale=2.61; 1080x1920) AppleWebKit/420+',
+					authority: 'i.instagram.com',
+					cookie: `ds_user_id=${getFromCookieFile('ds_user_id')}; sessionid=${getFromCookieFile('sessionid')}; csrftoken=${getFromCookieFile('csrftoken')};`
+				}
+			}))
+			const account = users.filter( ({ username }) => igAccount === username ).pop()
+			if (account)
+				resolve(account.pk)
+			else
+				reject(`${igAccount}: Essa conta do instagram não existe`)
 		} catch (error) {
 			reject(error)
 		}
--- functions/fetchStories.js
+++ functions/fetchStories.js
@@ -1,25 +1,26 @@
 const fetchStories = async (account_name, brand_name) => {
 	try {
+		const getFromCookieFile = require('./getFromCookieFile')
 		const fetchAccountId = require('./fetchAccountId')
 		const account_id = await fetchAccountId(account_name)
-		const getFromCookieFile = require('./getFromCookieFile')
 		const parameters = {
 			account_name: account_name,
 			account_id: account_id,
-			session_id: getFromCookieFile('sessionid'),
 			ds_user_id: getFromCookieFile('ds_user_id'),
+			session_id: getFromCookieFile('sessionid'),
+			csrftoken: getFromCookieFile('csrftoken'),
 			url: `https://i.instagram.com/api/v1/feed/user/${account_id}/story/`
 		}
 		const requestPromise = require('request-promise-native')
-		const response = await requestPromise({
-			url: parameters.url,
-			headers: {
-				'x-ig-capabilities': '3w==',
-				'user-agent': 'Instagram 9.5.1 (iPhone9,2; iOS 10_0_2; en_US; en-US; scale=2.61; 1080x1920) AppleWebKit/420+',
-				host: 'i.instagram.com',
-				cookie: `sessionid=${parameters.session_id}; ds_user_id=${parameters.ds_user_id}`
-			}
-		})
+		const response = await requestPromise({ 
+				url: parameters.url,
+				headers: {
+					'x-ig-capabilities': '3brTAw==',
+					'user-agent': 'Instagram 10.26.0 (iPhone7,2; iOS 10_1_1; en_US; en-US; scale=2.61; 1080x1920) AppleWebKit/420+',
+					authority: 'i.instagram.com',
+					cookie: `ds_user_id=${parameters.ds_user_id}; sessionid=${parameters.session_id}; csrftoken=${parameters.csrftoken};`
+				}
+			})
 		if (JSON.parse(response).reel !== null) {
 			const media = JSON.parse(response).reel.items
 			const images = media.map( (item) => {
